c0c362e7a4a266825474615f771ecae4de57ad6f
Steve Sisney <steve@archive.org>
2019-12-11 22:56:42 +0000

    Added long description and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as rfh:
+    long_description = rfh.read()
+
 setup(
     author='Steve Sisney',
     author_email='steve@archive.org',
-    data_files=[('', 'settings.yml')],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     description='Scrapy WARC I/O',
     install_requires=['python-magic', 'pyyaml', 'scrapy', 'warctools'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     name='scrapy_warcio',
     packages=find_packages(),
     url='https://github.com/internetarchive/scrapy_warcio',
